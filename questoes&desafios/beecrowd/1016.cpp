032fa3e19c34784ac49197c17d050f2d955ee8a1
Raimundo de Oliveira Coutinho Neto <119614670+coutinhoneto16@users.noreply.github.com>
2025-06-20 13:54:42 -0300

    Add files via upload

---
+++ questoes&desafios/beecrowd/1016.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int distancia;
+    cin >> distancia;
+    int tempoNecessario = distancia * 2;
+    printf("%d minutos\n",tempoNecessario);
+}
